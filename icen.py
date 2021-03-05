32de3df7c4aaf64ebef80a63f64fe5ecf0c6b406
RomelP <eprincipe@pucp.pe>
2021-03-04 20:06:11 -0500

    uso de librerias graficando

--- icen.py
+++ icen.py
@@ -1,9 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
+import pandas as pd
+import seaborn as sbn # 2
+import matplotlib.pyplot as plt
+#-------------------------------------------
 url = "http://met.igp.gob.pe/datos/icen.txt"
 
 # Obtener el contenido
 content = requests.get(url).content
 print(content)
-# extrae datos y convetir a lista 
+
+# extraer datos y convetir a lista
+
+# plotear ICEN 
